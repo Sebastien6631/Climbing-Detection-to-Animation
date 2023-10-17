56d9693488a83b8dcaf73051cfdfa3c604371dc2
Sébastienn Veyssiere <veyssieresebastien66@gmail.com>
2023-10-17 15:46:41 -0400

    Premier commit

--- PoseEstimation.py
+++ PoseEstimation.py
@@ -8,7 +8,7 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
-#Source : https://github.com/google/mediapipe/blob/master/docs/solutions/pose.md
+#Source: https://github.com/google/mediapipe/blob/master/docs/solutions/pose.md
 
 pTime = 0
 
